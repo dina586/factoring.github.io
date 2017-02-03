bc047920459bfee2eeb3c09f836382737e1b648e
dina zmit <dino4ka586@gmail.com>
2017-02-03 11:13:13 +0200

    create factoring03022017

---
+++ js/main.jquery.js
@@ -0,0 +1,10 @@
+$(document).ready(function(){
+  $('#j-slider').slick({
+	   dots: true
+  });
+  
+  $('#j-slider_opinion').slick({
+	   prevArrow: '<button type="button" class="slick-j-prev"></button>',
+	   nextArrow: '<button type="button" class="slick-j-next"></button>'
+  });
+});
